00660b2e65215cf802c8f9a45169cb367d9d0877
dela <dkagbodza@st.ug.edu.gh>
2022-12-08 15:39:19 +0000

    added a clear function to the script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+let addbtn = document.getElementById('addbtn');
+let clearbtn = document.getElementById('clearbtn');
+let mylist = document.getElementbyID('mylist');
 
+var todolistarray = [];
+
+var clearlist = function(){
+ todolistarray = function()
+todolistarray = []
+localStorage.removeItem('mylist');
+mylist.innerHTML = '';
+ }
+
+clearbtn.addEventListener('click',clearlist);
